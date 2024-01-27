c32f2fa292ce339912709fb02e552b40bf50c0e0
Saiman Khatiwada <saimankhatiwada9611@gmail.com>
2024-01-27 14:24:33 +0545

    setup

---
+++ src/Todo.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "todo API v1");
+        c.RoutePrefix= String.Empty;
+    });
+}
+
+app.MapGet("/hello", () => {
+    return "Hello World!";
+});
+
+app.Run();
+
